0ca89305202d1adfa4b2d52e14a8050018e4ee35
oriel <orielmalihi1@gmail.com>
2020-01-23 15:58:37 +0700

    testers added

--- src/Tests/MyGameGUITest.java
+++ src/Tests/MyGameGUITest.java
@@ -2,11 +2,25 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import Server.Game_Server;
+import Server.game_service;
+import dataStructure.DGraph;
+import dataStructure.Edge;
+import dataStructure.edge_data;
+import dataStructure.graph;
+import gameClient.MyGameGUI;
+import utils.Point3D;
+
 class MyGameGUITest {
-	private 
+	private game_service game = Game_Server.getServer(0);
+	private MyGameGUI gui = new MyGameGUI();
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -14,32 +28,33 @@ class MyGameGUITest {
 
 	@Test
 	void testGetWidth() {
-		fail("Not yet implemented");
+		int width = gui.getWidth();
+		assertEquals(width, 1400);
 	}
 
 	@Test
 	void testSetScale() {
-		fail("Not yet implemented");
+		game.addRobot(9);
+		game.startGame();
+		List<String> list = game.getFruits();
+		try {
+			JSONObject line = new JSONObject(list.get(0));
+			JSONObject fruit = line.getJSONObject("Fruit");
+			Point3D p = new Point3D(fruit.getString("pos"));
+			Point3D after = gui.setScale(p);
+			if(p.x()<0 || p.x()>1400 || p.y()<0 || p.y()>600)
+				fail("set scale did not work");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		game.stopGame();
 	}
 
 	@Test
 	void testGetGraph() {
-		fail("Not yet implemented");
+		graph g = gui.getGraph();
+		DGraph dg = new DGraph();
+		assertEquals(g, dg);
 	}
-
-	@Test
-	void testGetEdgeOfFruit() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testIsFruit() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetMissionList() {
-		fail("Not yet implemented");
-	}
-
 }
